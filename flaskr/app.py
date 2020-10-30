7c1b3bcb01ec9df2cdc696cd3645a4ee55b5cc64
Johnny <jhume04@qub.ac.uk>
2020-10-30 07:39:48 +0000

    Added Bootstrap
    Now there is a graph which will display values

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,15 +1,23 @@
-from data.connection import *
+from data.connection import get_all_data
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/all")
-def hello_world():
+def all_data_page():
     file_path = "C:/SideProjectsCode/SurfReport/flaskr/data/pipeline_616_forecast.json"
     allDataList = get_all_data(file_path)
+    print(type(allDataList[1]["timestamp"]))
     return render_template("home.html", all_data=allDataList)
 
 
+@app.route("/index")
+def index_testing():
+    file_path = "C:/SideProjectsCode/SurfReport/flaskr/data/pipeline_616_forecast.json"
+    allDataList = get_all_data(file_path)
+    return render_template("index.html", allData=allDataList)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- flaskr/data/connection.py
+++ flaskr/data/connection.py
@@ -6,6 +6,7 @@ def get_all_data(file_path):
     try:
         with open(file_path) as file:
             data = json.load(file)
+            #return format_data_for_table(data)
             return data
     except:
         return "Error, could not find JSON"
@@ -23,3 +24,11 @@ def get_wind_data(file_path):
 def time_stamp_to_date(timestamp):
     dt_object = datetime.fromtimestamp(timestamp)
     return dt_object
+
+def format_data_for_table(allData):
+    for dataDict in allData:
+        dataDict["timestamp"] = time_stamp_to_date(dataDict["timestamp"])
+        del dataDict["localTimestamp"]
+        del dataDict["issueTimestamp"]
+
+    return allData
